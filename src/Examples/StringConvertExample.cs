91b86911ee618830683c40497c072256910bf1b5
st_Akey <qwer0516yp@gmail.com>
2024-03-06 13:36:36 +1100

    Add AES GCM encryption and decryption example

--- src/Examples/StringConvertExample.cs
+++ src/Examples/StringConvertExample.cs
@@ -3,15 +3,24 @@ using Swashbuckle.AspNetCore.Filters;
 
 namespace CipherCracker.Api.Examples;
 
-public class StringConvertExample : IExamplesProvider<StringConvertRequest>
+public class StringConvertExample : IMultipleExamplesProvider<StringConvertRequest>
 {
-    public StringConvertRequest GetExamples()
+    public IEnumerable<SwaggerExample<StringConvertRequest>> GetExamples()
     {
-        return new StringConvertRequest 
-        {
-            From = "hex",
-            To = "base64",
-            Text = "1234567890abcdefABCDEF"
-        };
+        yield return SwaggerExample.Create("hex to base64", 
+            new StringConvertRequest
+            {
+                From = "hex",
+                To = "base64",
+                Text = "1234567890abcdefABCDEF"
+            });
+
+        yield return SwaggerExample.Create("base64 to hex",
+            new StringConvertRequest
+            {
+                From = "base64",
+                To = "hex",
+                Text = "EjRWeJCrze+rze8="
+            });
     }
 }
